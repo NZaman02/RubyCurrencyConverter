2cb7181bfcb5035a683f20c13c97f201c8e35ab9
Nadvi Zaman <nadvi.zaman@student.manchester.ac.uk>
2023-02-17 18:09:26 +0000

    could be done

--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -10,28 +10,41 @@ module CurrencyExchange
     require 'json'
     jsonFile = File.read('./data/eurofxref-hist-90d.json')
     parsedFile = JSON.parse(jsonFile)
+    return self.performCalc(parsedFile,date,from_currency,to_currency)
 
+    #having an "easier to ask for forgiveness system" call the right function based
+    #on the input file that is seen
+  rescue Exception => e
+    print("Not a json")
+
+    end
+
+
+
+  def self.performCalc(parsedFile,date,from_currency,to_currency)
     #check date valid first
     if parsedFile[date.to_s] == nil
      # raise "The date was invalid"
     else
       #only tries to calculate if date true
+      fromCurrInt = parsedFile[date.to_s][from_currency.to_s]
+      toCurrInt = parsedFile[date.to_s][to_currency.to_s]
+
+      #everything is based on EUR so would just be 1
       if to_currency.to_s == "EUR"
         toCurrInt = 1
       end
       if from_currency.to_s == "EUR"
         fromCurrInt = 1
       end
-      fromCurrInt = parsedFile[date.to_s][from_currency.to_s]
-      toCurrInt = parsedFile[date.to_s][to_currency.to_s]
 
       #checks both currencies are valid
       if fromCurrInt == nil
-       # raise "The currency FROM is invalid"
+       raise "The currency FROM is invalid"
        return (1*toCurrInt)
 
       elsif toCurrInt == nil
-       # raise "The currency TO is invalid"
+       raise "The currency TO is invalid"
        return(1/fromCurrInt)
 
       else
@@ -41,8 +54,6 @@ module CurrencyExchange
       end
 
     end
-
   end
 
 end
-
